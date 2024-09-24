cd4ce483c336bb88f9964feb806d9d6dbf465c19
Your Name <your.email@example.com>
2024-09-24 12:59:09 +0530

    work on offline

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Offline Learning App</h1>
+      <p>Welcome to your offline learning platform!</p>
+      <ul  style={{listStyleType: "none"}}>
+        <li>Lesson 1: Introduction to HTML</li>
+        <li>Lesson 2: Introduction to CSS</li>
+        <li>Lesson 3: Basics of JavaScript</li>
+        <li>Lesson 4: Introduction to React</li>
+      </ul>
     </div>
+  </div>
   );
 }
 
